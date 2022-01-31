cdcaab3a9993d98e0d5901292a9c84171b7a9ed5
ElKaribooSama <matis.blondy@ynov.com>
2022-01-31 17:13:14 +0100

    dev syncing2

--- data-pull.go
+++ data-pull.go
@@ -27,24 +27,35 @@ type Artist struct {
 	Relations    string   `json:"relations"`
 }
 
-func main() {
-	var APIconfig API
+type Locations struct {
+	Index []struct {
+		Id        int      `json:"id"`
+		Locations []string `json:"locations"`
+	} `json:"index"`
+}
 
-	GetAPI()
-
-	jsonFile, err := os.Open("API.json")
-	defer jsonFile.Close()
-	if err != nil {
-		fmt.Println(err)
+type Date struct {
+	Index []struct {
+		Id   int      `json:"id"`
+		Date []string `json:"dates"`
 	}
+}
 
-	JsonReader := json.NewDecoder(jsonFile) // Decode the json file
-	err = JsonReader.Decode(&APIconfig)
+type Relation struct {
+	Index []struct {
+		Id             int                 `json:"id"`
+		DatesLocations map[string][]string `json:"datesLocations"`
+	} `json:"index"`
+}
 
-	/*GetAPI(APIconfig.Artists)
-	GetAPI(APIconfig.Locations)
-	GetAPI(APIconfig.Dates)
-	GetAPI(APIconfig.Locations)*/
+func main() {
+	apiUrl := GetAPI()
+	fmt.Print(apiUrl)
+
+	GetData(apiUrl.Artists, "json/artist.json", "Artist")
+	GetData(apiUrl.Locations, "json/locations.json", "Locations")
+	GetData(apiUrl.Dates, "json/dates.json", "Date")
+	GetData(apiUrl.Relation, "json/relation.json", "Relation")
 }
 
 func GetAPI() API {
@@ -55,11 +66,78 @@ func GetAPI() API {
 	defer resp.Body.Close()
 
 	html, err := ioutil.ReadAll(resp.Body)
-
 	var apiUrl API
 	err = json.Unmarshal(html, &apiUrl)
 	if err != nil {
 		panic(err)
 	}
 	return apiUrl
+}
+
+func GetData(url, path, usedStruct string) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	html, err := ioutil.ReadAll(resp.Body)
+
+	switch usedStruct {
+	case "Artist":
+		var APITest []Artist
+		err = json.Unmarshal(html, &APITest)
+		if err != nil {
+			panic(err)
+		}
+		file, err := os.Create(path) // Create a json file
+		if err != nil {
+			return
+		}
+		FileData, _ := json.MarshalIndent(APITest, "", " ") // Encode the json file
+		_ = ioutil.WriteFile(path, FileData, 0644)          // Write the variable in the json file
+		defer file.Close()
+
+	case "Locations":
+		var APITest Locations
+		err = json.Unmarshal(html, &APITest)
+		if err != nil {
+			panic(err)
+		}
+		file, err := os.Create(path) // Create a json file
+		if err != nil {
+			return
+		}
+		FileData, _ := json.MarshalIndent(APITest, "", " ") // Encode the json file
+		_ = ioutil.WriteFile(path, FileData, 0644)          // Write the variable in the json file
+		defer file.Close()
+
+	case "Date":
+		var APITest Date
+		err = json.Unmarshal(html, &APITest)
+		if err != nil {
+			panic(err)
+		}
+		file, err := os.Create(path) // Create a json file
+		if err != nil {
+			return
+		}
+		FileData, _ := json.MarshalIndent(APITest, "", " ") // Encode the json file
+		_ = ioutil.WriteFile(path, FileData, 0644)          // Write the variable in the json file
+		defer file.Close()
+
+	case "Relation":
+		var APITest Relation
+		err = json.Unmarshal(html, &APITest)
+		if err != nil {
+			panic(err)
+		}
+		file, err := os.Create(path) // Create a json file
+		if err != nil {
+			return
+		}
+		FileData, _ := json.MarshalIndent(APITest, "", " ") // Encode the json file
+		_ = ioutil.WriteFile(path, FileData, 0644)          // Write the variable in the json file
+		defer file.Close()
+	}
 }
